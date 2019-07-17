66e31626b801d66efaa8d9f1eedc9f77ed6b691a
Austin <austin.gray4292@gmail.com>
2019-07-17 12:48:16 -0700

    Fixes error handling

--- server/pokemon_controller.ts
+++ server/pokemon_controller.ts
@@ -12,21 +12,26 @@ interface Pokemon {
 
 export default class PokemonController {
   async index(request: Hapi.Request, h: Hapi.ResponseToolkit) {
-    const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=807').catch(err => err);
-    const pokedata = res.data;
-    if (pokedata) {
+    try {
+      const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=807');
+      const pokedata = res.data;
       const meta: { count: number } = { count: pokedata.results.length };
       const items: Pokemon[] = pokedata.results;
       const byId: ById<Pokemon> = {};
       items.forEach((pokemon, i) => Object.assign(byId, { [i+1]: pokemon }));
       return { meta, items, byId };
-    } else return h.response('Not found').code(404);
+    } catch (err) {
+      return h.response('Not found').code(404);
+    }
   }
   async show(request: Hapi.Request, h: Hapi.ResponseToolkit) {
     const search = request.params.poke;
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`).catch(err => err);
-    const pokedata = res.data;
-    if (pokedata) return pokedata;
-    else return h.response('Not found').code(404);
+    try {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`);
+      const pokedata = res.data;
+      return pokedata;
+    } catch (err) {
+      return h.response('Not found').code(404);
+    }
   }
 }
